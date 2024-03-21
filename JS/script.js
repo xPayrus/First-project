d22bdb7278365bbf252740e130aab3f75dab48f4
Adrian Cardenas <adriancardenas1050@gmail.com>
2024-03-21 14:35:47 -0500

    primer proyecto

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+function mostrarCartel1() {
+  alert("Tocaste el boton");
+}
+
+function mostrarCartel2() {
+  alert("Tocaste el boton");
+}
+
+const boton1 = document.getElementById("Compro");
+const boton2 = document.getElementById("Favorito");
+
+function mostrarCartel1() {
+  boton1.innerHTML = "Compro el producto!!!";
+}
+
+function mostrarCartel2() {
+  boton2.innerHTML = "Añadió a favoritos!!!";
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+My first proyect
+codigo basico 
